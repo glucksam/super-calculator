ba9a0bd605a9e7bb23eacb96fb359840c0ce65a1
adiglucksam <adiglucksam@741fb740-f291-d1f2-be72-f2733ba065f9>
2011-10-05 16:36:09 +0000

    Added tester to matrix and a base for general testing.

---
+++ trunk/Classes/MatrixTester.h
@@ -0,0 +1,25 @@
+//
+//  MatrixTester.h
+//  unsigned_calc
+//
+//  Created by Adi Glucksam on 5/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Matrix.h"
+
+@interface MatrixTester : NSObject {
+
+}
+/*private functions*/
++(bool) testDet:(Matrix*)mat;
++(bool) testTridiagonal:(Matrix*) mat;
++(bool) testInverse:(Matrix*) mat;
++(void) PrintError:(NSString*)sError:(Matrix*)mat;
+/*public functions*/
++(bool) TestWith:(Matrix*) mat;
++(bool) RandomTests;
++(bool) SanityTest;
+
+@end
