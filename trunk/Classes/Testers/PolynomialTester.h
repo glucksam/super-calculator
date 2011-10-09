abc8123c4b12843e640dc06aca2c81b65ccbf6c1
adiglucksam <adiglucksam@741fb740-f291-d1f2-be72-f2733ba065f9>
2011-10-09 09:54:21 +0000

    Part three- added tester to test polynomials

---
+++ trunk/Classes/Testers/PolynomialTester.h
@@ -0,0 +1,25 @@
+//
+//  PolynomialTester.h
+//  unsigned_calc
+//
+//  Created by Adi Glucksam on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Polynom.h"
+
+@interface PolynomialTester : NSObject {
+
+}
+/*private functions*/
++(bool) testMultiply:(Polynom*)p:(Polynom*)q;
++(bool) testDivide:(Polynom*)p:(Polynom*)q;
++(bool) testAdd:(Polynom*)p:(Polynom*)q;
++(bool) testCompare:(Polynom*)p:(Polynom*)q;
++(bool) testGetRationalRoots:(Polynom*)p;
+/*public functions*/
++(bool) TestWith:(Polynom*)p:(Polynom*)q;
++(bool) RandomTests;
++(bool) SanityTest;
+@end
